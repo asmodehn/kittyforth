62abf69ca01538943bf9c64bc47034e9c3773b1a
AlexV <asmodehn@gmail.com>
2022-10-17 16:47:38 +0200

    add basic packages and readall() to test interactive julia

--- src/kittyforth.jl
+++ src/kittyforth.jl
@@ -2,4 +2,17 @@ module kittyforth
 
 greet() = print("Hello World!")
 
+function readall()
+
+    # CAREFUL : TODO first buffered or unbuffered terminal...
+
+    # TODO second curses compatibility ???
+
+    while !eof(stdin)
+        x = read(stdin, Char)  # we want char by char for customiation of terminal navigation
+        println("Found: $x")
+    end
+
+end
+
 end # module kittyforth
